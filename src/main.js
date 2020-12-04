34f4e4a7c206010993830caa2e1ed8f8ded93b3b
Pilar Chanampe <pchanampe@qplus.com.ar>
2020-12-04 11:42:51 -0300

    i18n example working

--- src/main.js
+++ src/main.js
@@ -7,11 +7,37 @@
 import Vue from 'vue'
 import App from './App.vue'
 import {VueFormBuilderPlugin} from "@/index";
+// import messages from './views/builder/prueba-locales.json';
+import VueI18n from 'vue-i18n';
 
-Vue.config.productionTip = false
 
-Vue.use(VueFormBuilderPlugin)
+Vue.config.productionTip = false;
+
+Vue.use(VueFormBuilderPlugin);
+Vue.use(VueI18n);
+
+const messages = {
+  en: {
+    message: {
+      hello: 'hello world'
+    },
+    // builderButtonCreate: 'Create'
+  },
+  ja: {
+    message: {
+      hello: 'こんにちは、世界'
+    },
+    builderButtonCreate: 'Crear'
+  }
+}
+
+// Create VueI18n instance with options
+const i18n = new VueI18n({
+  locale: 'ja', // set locale
+  messages, // set locale messages
+})
 
 new Vue({
+  i18n,
   render: h => h(App)
 }).$mount('#app')
