f1b4e32cc692c35d3bd3b7f7396c0603280499de
Osmel Guarepo <osmelg@hotmail.es>
2021-01-14 13:52:07 -0400

    Builder > Backup

--- src/configs/controls.js
+++ src/configs/controls.js
@@ -15,6 +15,7 @@ import {
 } from "@/configs/control-config-enum";
 
 // Control-GUI-Component
+import ImageControl from "@/views/controls/ImageControl"
 import InputControl from "@/views/controls/InputControl"
 import TextControl from "@/views/controls/TextControl"
 import ButtonControl from "@/views/controls/ButtonControl"
@@ -37,9 +38,18 @@ import TextConfigView from "@/views/control-configs/TextConfigView";
 import DropdownConfigView from "@/views/control-configs/DropdownConfigView";
 
 const CONTROLS = {
+    image: {
+        name: "Imagen name",
+        description: "Imagen descripcion",
+        icon: 'editPencil', // Follow ICON in `icon-facade.js` to see how it works.
+
+        // component mapping
+        fieldComponent: ImageControl
+    },  
+
     input: {
-        name: "Caja de texto",
-        description: "Caja de texto de una linea",
+        name: "Caja de textox",
+        description: "Caja de textox de una linea",
         icon: 'editPencil', // Follow ICON in `icon-facade.js` to see how it works.
 
         // component mapping
---
+++ src/views/controls/ImageControl.vue
@@ -0,0 +1,37 @@
+<template>
+    <div>
+        <select v-model="geturl" @change="addImage">
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+        </select>
+        <div v-if="seturl">
+            <img
+                :src="require(`../../assets/imagesbank/${seturl}.jpg`)"
+                alt="Imagen" 
+                style="height:200px; width:200px;">
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "ImageControl",
+    data(){
+        return{
+            seturl: '',
+            geturl: ''
+        }
+    },
+    methods:{
+        addImage(){
+            this.seturl = this.geturl;
+            console.log('done =o');
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
